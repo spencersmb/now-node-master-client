c12698f8c331ec3c41d6da7752c7791c78e06023
spencer <spencer.bigum@gmail.com>
2017-06-14 20:00:04 -0400

    added flow support

--- components/stores/reviews/reviewList.js
+++ components/stores/reviews/reviewList.js
@@ -1,12 +1,27 @@
-import { connect } from 'react-redux'
+// @flow
 import Review from './review'
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default connect()(props => {
+interface iReview {
+  _id: String,
+  store: String,
+  text: String,
+  rating: Number
+}
+
+type Props = {
+  reviews?: iReview[]
+}
+
+export default connect()(({ reviews }: Props) => {
   const renderStores = () => {
-    return props.reviews.map(review => {
-      return <Review key={review._id} {...review} />
-    })
+    if (reviews) {
+      return reviews.map(review => {
+        return <Review key={review._id} {...review} />
+      })
+    }
+    return <div>There are no reviews yet. </div>
   }
   return (
     <div className='reviews'>
